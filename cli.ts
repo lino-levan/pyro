0946b09f52107a1ac702b81be868c786cec19152
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-03-14 13:14:18 -0400

    feat: cli options

--- cli.ts
+++ cli.ts
@@ -6,7 +6,7 @@ import { build } from "./src/build.ts";
 
 await new Command()
   .name("pyro")
-  .version("0.1.0")
+  .version("0.2.1")
   .description("The documentation site generator for Deno")
   .command("gen", "Generate a Pyro site.")
   .arguments("<path:string>")
@@ -14,8 +14,13 @@ await new Command()
     generate(path.resolve(p));
   })
   .command("dev", "Start the Pyro dev server")
-  .action(() => {
-    dev();
+  .option("-p, --port <value:number>", "Specify a port for the the dev server")
+  .option(
+    "-n, --hostname <value:string>",
+    "Specify a hostname for the dev server",
+  )
+  .action(({ port, hostname }) => {
+    dev(hostname, port);
   })
   .command("build", "Build the static Pyro site")
   .action(() => {
