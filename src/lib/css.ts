7eb5b741d4bfd27ea33ffb977ef3f32296e72ee3
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-03-14 12:36:53 -0700

    fix: light mode

--- src/lib/css.ts
+++ src/lib/css.ts
@@ -51,35 +51,6 @@ details > summary::-webkit-details-marker {
   --color-accent-emphasis:#0969da;
   --color-danger-fg:#cf222e
 }
-@media (prefers-color-scheme:light){
-  .markdown-body {
-      --color-canvas-default-transparent:rgba(13,17,23,0);
-      --color-prettylights-syntax-comment:#8b949e;
-      --color-prettylights-syntax-constant:#79c0ff;
-      --color-prettylights-syntax-entity:#d2a8ff;
-      --color-prettylights-syntax-storage-modifier-import:#c9d1d9;
-      --color-prettylights-syntax-entity-tag:#7ee787;
-      --color-prettylights-syntax-keyword:#ff7b72;
-      --color-prettylights-syntax-string:#a5d6ff;
-      --color-prettylights-syntax-variable:#ffa657;
-      --color-prettylights-syntax-string-regexp:#7ee787;
-      --color-prettylights-syntax-markup-deleted-text:#ffdcd7;
-      --color-prettylights-syntax-markup-deleted-bg:#67060c;
-      --color-prettylights-syntax-markup-inserted-text:#aff5b4;
-      --color-prettylights-syntax-markup-inserted-bg:#033a16;
-      --color-prettylights-syntax-constant-other-reference-link:#a5d6ff;
-      --color-fg-default:#c9d1d9;
-      --color-fg-muted:#8b949e;
-      --color-canvas-default:#0d1117;
-      --color-canvas-subtle:#161b22;
-      --color-border-default:#30363d;
-      --color-border-muted:#21262d;
-      --color-neutral-muted:rgba(110,118,129,.4);
-      --color-accent-fg:#58a6ff;
-      --color-accent-emphasis:#1f6feb;
-      --color-danger-fg:#f85149
-  }
-}
 @media (prefers-color-scheme:dark){
   .markdown-body {
       --color-canvas-default-transparent:rgba(13,17,23,0);
