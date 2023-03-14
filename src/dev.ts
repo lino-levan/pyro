aa4dfb6b3fd3d4f56002d2e19913af81ce2d3254
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-03-14 14:09:00 -0400

    feat: cli options (#14)
    Co-authored-by: Lino Le Van <11367844+lino-levan@users.noreply.github.com>

--- src/dev.ts
+++ src/dev.ts
@@ -7,7 +7,7 @@ import { CSS } from "./lib/css.ts";
 import { parse } from "std/encoding/yaml.ts";
 import { Config } from "./lib/types.ts";
 
-export function dev() {
+export function dev(hostname = "0.0.0.0", port = 8000) {
   const config = parse(Deno.readTextFileSync("pyro.yml")) as Config;
 
   serve(async (req) => {
@@ -46,5 +46,8 @@ export function dev() {
         },
       },
     );
+  }, {
+    hostname,
+    port,
   });
 }
